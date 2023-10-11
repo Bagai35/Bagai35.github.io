157159fd4216be9028298e09b4d724edf5a9e5d5
Bogdan Keplast <60263970+Bagai35@users.noreply.github.com>
2023-10-11 17:35:39 +0300

    Add files via upload

---
+++ x-parody/precache-manifest.42c960971515644d46356771b5543662.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "00d1af8ee858cfb03889",
+    "url": "/x-parody/static/css/main.bf5af1c5.chunk.css"
+  },
+  {
+    "revision": "00d1af8ee858cfb03889",
+    "url": "/x-parody/static/js/main.00d1af8e.chunk.js"
+  },
+  {
+    "revision": "e60329008bd4d3cfd859",
+    "url": "/x-parody/static/css/1.b69cf8ba.chunk.css"
+  },
+  {
+    "revision": "e60329008bd4d3cfd859",
+    "url": "/x-parody/static/js/1.e6032900.chunk.js"
+  },
+  {
+    "revision": "8b31c7427f8c11e46685",
+    "url": "/x-parody/static/js/runtime~main.8b31c742.js"
+  },
+  {
+    "revision": "f2ad42768ed2778d3af7953714fa351e",
+    "url": "/x-parody/index.html"
+  }
+];
